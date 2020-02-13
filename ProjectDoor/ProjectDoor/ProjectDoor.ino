50f28564debca9564baae1b84c1f58fb49b3d7cc
Mirko Piitulainen <mirko.piitulainen@hotmail.com>
2020-02-13 13:33:13 +0200

    Update ProjectDoor.ino
    We got the connection to work! HYPERS

--- ProjectDoor/ProjectDoor/ProjectDoor.ino
+++ ProjectDoor/ProjectDoor/ProjectDoor.ino
@@ -1,13 +1,3 @@
-
-
-  
-
-const int sck = 9; 
-const int mosi = 8; 
-const int cs = A3; 
-const int res = A2; 
-const int dc = A1; 
-
 // Définition of colors 
 #define BLACK 0x0000 //musta 
 #define BLUE 0x001F //sininen 
@@ -23,123 +13,118 @@ const int dc = A1;
 #include <Adafruit_GFX.h> 
 #include <Adafruit_SSD1331.h> 
 #include <SPI.h> 
+#include <WiFi101.h>
+#include <WiFiSSLClient.h>
+#include <MQTTClient.h> 
+
+const int sck = 9; 
+const int mosi = 8; 
+const int cs = A3; 
+const int res = A2; 
+const int dc = A1; 
 
 Adafruit_SSD1331 afficheur = Adafruit_SSD1331(cs, dc, mosi, sck, res); //création of object 
 
-  
+// WLAN. PSA; When trying to get this to work on your network, change the SSID AND PASS!!!!
+char ssid[] = "Machine's Android"; //  your network SSID (name)
+char pass[] = "chogathop";    // your network password (use for WPA)
 
-void setup(void) 
+//IBM Watson definitions for the Arduino.
+//char *client_id = "d:<your Organization ID>:<your Device Type>:<your Device ID>"; 
+char *client_id = "d:pm8jn8:Arduino_MKR1000:AMKR1000";
+char *user_id = "use-token-auth";   // telling that authentication will be done with token
+char *authToken = "HomoSapiens3000"; // Your IBM Watson Authentication Token
+
+//char *ibm_hostname = "<your-org-id.messaging.internetofthings.ibmcloud.com>";
+char *ibm_hostname = "pm8jn8.messaging.internetofthings.ibmcloud.com";
+
+
+
+WiFiClient net;
+MQTTClient MQTTc;
+
+unsigned long lastMillis = 0;
+
+
+void setup() 
 { 
-afficheur.begin(); // initialization of display objcet 
-  pinMode (1, INPUT); 
-  Serial.begin(9600); 
+  afficheur.begin(); // initialization of display objcet 
+  pinMode (1, INPUT); //EXCUSE ME, WHat is this???? 
+  Serial.begin(9600); //Initilization of the serial monitor. Shows if you are connected to the IBM Cloud
+  WiFi.begin(ssid, pass);
+  Serial.println("Connected to WLAN");
+  printWiFiStatus();
+  
+//Starts the MQTT broker and connects.
+  
+  MQTTc.begin(ibm_hostname, 1883, net);
+  connect();
+  
 } 
 
   
 
 void loop() 
 { 
-Serial.println(digitalRead(1)); 
-if (digitalRead(1) == 0) 
 
-{ 
+MQTTc.loop(); 
+if(millis() - lastMillis > 10000) 
+     {
+        Serial.println(digitalRead(1));
+        if (digitalRead(1) == 0) 
+          { 
+          afficheur.fillScreen(BLACK); // background screen in black 
+          afficheur.setTextColor(SKY); // color of text in cyan 
+          afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
+          afficheur.setTextSize(4); 
+          afficheur.print("MAIL"); // display text   
+          } 
+    
+          else 
+          { 
+          afficheur.fillScreen(BLACK); // background screen in black 
+          afficheur.setTextColor(SKY); // color of text in cyan 
+          afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
+          afficheur.setTextSize(2); 
+          afficheur.print("Waiting"); // display text   
+          } 
+       lastMillis = millis();
+     }
 
-afficheur.fillScreen(BLACK); // background screen in black 
-afficheur.setTextColor(SKY); // color of text in cyan 
-afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
-afficheur.setTextSize(4); 
-afficheur.print("MAIL"); // display text   
-} 
+}
+//Connection establisment
+void connect() 
+{
+  Serial.print("checking WLAN...");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.println("Connecting WLAN...  ");
+    Serial.print(".");
+    delay(500);
+  }
+   Serial.print("\nconnecting Watson with MQTT....");
+  while (!MQTTc.connect(client_id,user_id,authToken)) 
+  {
+    Serial.println("Connecting mqtt broker...  ");
+    Serial.print(".");
+    delay(10000);
+  }
+  Serial.println("\nconnected!");
+}
 
-else 
-{ 
-afficheur.fillScreen(BLACK); // background screen in black 
-afficheur.setTextColor(SKY); // color of text in cyan 
-afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
-afficheur.setTextSize(2); 
-afficheur.print("Waiting"); // display text   
-} 
 
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
   
 
   
 
-/* afficheur.fillScreen(BLACK); // background screen in black 
-
-afficheur.setTextColor(SKY); // color of text in cyan 
-
-afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
-
-afficheur.setTextSize(4); 
-
-afficheur.print("YOU"); // display text 
-
-delay(500); // wait 500 ms 
-
-afficheur.fillScreen(BLACK); // background screen in black 
-
-afficheur.setTextColor(SKY); // color of text in cyan 
-
-afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
-
-afficheur.setTextSize(4); 
-
-afficheur.print("GOT"); // display text 
-
-delay(500); // wait 500 ms 
-
-afficheur.fillScreen(BLACK); // background screen in black 
-
-afficheur.setTextColor(SKY); // color of text in cyan 
-
-afficheur.setCursor(0,20); // cursor is in x=0 and y=15 
-
-afficheur.setTextSize(4); 
-
-afficheur.print("MAIL"); // display text 
-
-delay(500); // wait 500 ms 
-
-/* 
-
-//afficheur.setCursor(0,15); // cursor is in x=0 and y=15 
-
-//afficheur.setTextSize(2); // size of text 
-
-//afficheur.setTextColor(ROUGE); // text in red color 
-
-//afficheur.println("DIGILENT"); // display text 
-
-//afficheur.setCursor(20,40); // cursor is in x=20 and y=40 
-
-//afficheur.setTextSize(1); // size of text 
-
-//afficheur.setTextColor(VERT); // text in green color 
-
-//afficheur.println("LEXTRONIC"); // display text 
-
-//afficheur.drawFastHLine(1, 60, afficheur.width()-1, BLEU); // blue line x=1 to screen width-1 and y=60 
-
-delay(2000); // wait 2 s 
-
-afficheur.fillScreen(ORANGE); // background display in black (erase display) 
-
-  
-
-afficheur.fillRoundRect(0, 0, 96, 10.666, 0, RED); // (x, y, leveys, korkeus, pyöreys) 
-
-afficheur.fillRoundRect(0, 10.666, 96, 10.666, 0, ORANGE); 
-
-afficheur.fillRoundRect(0, 21.333, 96, 10.666, 0, YELLOW); 
-
-afficheur.fillRoundRect(0, 32, 96, 10.666, 0, GREEN); 
-
-afficheur.fillRoundRect(0, 42.666, 96, 10.666, 0, BLUE); 
-
-afficheur.fillRoundRect(0, 53.333, 96, 10.666, 0, PURPLE); 
-
-//afficheur.fillCircle(90, 55, 5, JAUNE); // yellow circle with radius=5 in=90 and y=55 
-
-delay(1000); */ 
 
 } 
